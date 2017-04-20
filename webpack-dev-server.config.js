9386d1d587345f1d7bebb7c5106514e2ca8823a6
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-04-20 23:12:56 +0200

    [npm] Upgrade to webpack@2

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -1,20 +1,19 @@
 const webpack = require('webpack');
 const path = require('path');
-const buildPath = path.resolve(__dirname, 'build');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
 
 const config = {
   // Entry points to the project
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, '/src/app/app.js'),
-  ],
+  entry: {
+    main: [
+      // only- means to only hot reload for successful updates
+      'webpack/hot/only-dev-server',
+      './src/app/app.js',
+    ],
+  },
   // Server Configuration options
   devServer: {
     contentBase: 'src/www', // Relative directory for base of server
-    devtool: 'eval',
     hot: true, // Live-reload
     inline: true,
     port: 3000, // Port Number
@@ -22,25 +21,26 @@ const config = {
   },
   devtool: 'eval',
   output: {
-    path: buildPath, // Path of output file
+    path: path.resolve(__dirname, 'build'), // Path of output file
     filename: 'app.js',
   },
   plugins: [
     // Enables Hot Modules Replacement
     new webpack.HotModuleReplacementPlugin(),
-    // Allows error warnings but does not stop compiling.
-    new webpack.NoErrorsPlugin(),
     // Moves files
     new TransferWebpackPlugin([
       {from: 'www'},
     ], path.resolve(__dirname, 'src')),
   ],
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js$/, // All .js files
-        loaders: ['babel-loader'],
-        exclude: [nodeModulesPath],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+        },
       },
     ],
   },
--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,16 +1,18 @@
 const webpack = require('webpack');
 const path = require('path');
-const buildPath = path.resolve(__dirname, 'build');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
 
 const config = {
-  entry: [path.join(__dirname, '/src/app/app.js')],
+  entry: {
+    main: [
+      './src/app/app.js',
+    ],
+  },
   // Render source-map file for final build
   devtool: 'source-map',
   // output config
   output: {
-    path: buildPath, // Path of output file
+    path: path.resolve(__dirname, 'build'), // Path of output file
     filename: 'app.js', // Name of output file
   },
   plugins: [
@@ -22,24 +24,22 @@ const config = {
     }),
     // Minify the bundle
     new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        // suppresses warnings, usually from module minification
-        warnings: false,
-      },
+      sourceMap: true,
     }),
-    // Allows error warnings but does not stop compiling.
-    new webpack.NoErrorsPlugin(),
     // Transfer Files
     new TransferWebpackPlugin([
       {from: 'www'},
     ], path.resolve(__dirname, 'src')),
   ],
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js$/, // All .js files
-        loaders: ['babel-loader'], // react-hot is like browser sync and babel loads jsx and es6-7
-        exclude: [nodeModulesPath],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+        },
       },
     ],
   },
